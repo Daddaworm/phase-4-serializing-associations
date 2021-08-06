35ce14c6858d1c524ace120d2c93f5c98022b203
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-08-06 11:38:50 -0400

    commit tree c70fabb2a3cf33e89f0b5309698185c3581b6069

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,4 +1,7 @@
 class MovieSerializer < ActiveModel::Serializer
   attributes :id, :title, :year, :length, :description, :poster_url, :category, :discount
+
+  belongs_to :director # This will nest a Director object inside the Movies object
+  has_many :reviews
   
 end
